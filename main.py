bb5e54760e01c5d1dcecf631bd5c64344a095685
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-01-03 15:38:03 +0000

    feat: initial setup

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi():
+    # Use a breakpoint in the code line below to debug your script.
+    dog_icon = '\U0001F436'
+    print(f"{dog_icon} Welcome to Storm's Woofeteria. {dog_icon}")  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
