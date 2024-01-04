92aff68b820b8064f139b8d0c90e938e3013686b
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-01-04 16:25:13 +0000

    feat: update original stock list

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import copy
+
 from infrastructure.enums.enum_icon import Icon
 from services.cafeteria_item_service import CafeteriaItemService
 from services.cart_service import CartService
@@ -15,10 +17,14 @@ def start_cafeteria():
           "If you wish to order more than one item please separate each item number by comma.")
     user_input = input()
     item_ids = [int(x) for x in user_input.split(',') if x.strip().isdigit()]
-    selected_menu_items = [item for item in menu_list if item.Id in item_ids]
+    menu_list_copy = copy.deepcopy(menu_list)
+    selected_menu_items = [item for item in menu_list_copy if item.Id in item_ids]
+    selected_menu_items_original = [item for item in menu_list if item.Id in item_ids]
     for item in selected_menu_items:
         user_input = input(f"How many {item.Name} would you like to order?")
         item.Stock = int(user_input)
+    for item in selected_menu_items_original:
+        item.Stock = item.Stock - int(user_input)
     cart = cart_service.add_to_cart(selected_menu_items)
     print("Are you finished with your order? (Y/N)")
     user_input = input().capitalize()
@@ -34,6 +40,9 @@ def start_cafeteria():
             user_input = input()
             item_ids = [int(x) for x in user_input.split(',') if x.strip().isdigit()]
             selected_menu_items = [item for item in menu_list if item.Id in item_ids]
+            for item in selected_menu_items:
+                user_input = input(f"How many {item.Name} would you like to order?")
+                item.Stock = int(user_input)
             cart.Items.extend(selected_menu_items)
             cart = cart_service.update_cart(cart, cart.Items)
             cart_service.print_cart(cart)
